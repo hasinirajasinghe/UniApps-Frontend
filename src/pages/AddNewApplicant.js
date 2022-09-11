166f4db424806cfc09d968da651caa7eba520b9d
hasinirajasinghe <hasinichathu@gmail.com>
2022-09-11 15:22:40 -0400

    applicant create working

--- src/pages/AddNewApplicant.js
+++ src/pages/AddNewApplicant.js
@@ -1,45 +1,62 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 import { useNavigate } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import { Button, Toast } from "react-bootstrap";
 import axios from "axios";
 
-const AddNewApplicant = () => {
+const AddNewApplicant = ({addNewApplicant}) => {
     const initialState = {
         name: "",
         email: "",
         phone_number: "",
         major: "",
         enrollment_status: "",
-    };
+    }
 
+    const [show, setShow] = useState(false)
+    const [toastMessage, setToastMessage] = useState('')
     const navigate = useNavigate();
     const [formData, setFormData] = useState(initialState);
 
     const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.id]: e.target.value });
-    };
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        axios.post("http://localhost:8000/applicants", formData).then((res) => {
-            setFormData(initialState);
-            navigate("/dashboard", { replace: true });
-        });
-    };
+        axios.post("http://localhost:8000/applicants/", formData).then((res) => {
+            if (res.status >= 200 && res.status < 300) {
+                addNewApplicant(res.data)
+                navigate("/dashboard", { replace: true })
+              } else {
+                setToastMessage('Failed to create applicant!')
+                setShow(true)
+              }
+          })
+          .catch(error => {
+            setToastMessage('Encountered error while creating applicant!')
+            setShow(true)
+          })
+    }
+
+    const majors = useRef([])
+    const enrollment_statuses = useRef([])
 
     useEffect(() => {
-        fetch('http://localhost:8000/majors/')
-        .then(res => res.json())
-        .then(data => majors.current = data)
-        .catch(error => console.log(error))
-
-        fetch('http://localhost:8000/enrollment-statuses/')
-        .then(res => res.json())
-        .then(data => enrollment_statuses.current = data)
-        .catch(error => console.log(error))
-    }, [id])
+        Promise.all([
+            fetch("http://localhost:8000/majors/"),
+            fetch("http://localhost:8000/enrollment-statuses/"),
+          ]).then(([majors, enrollment_statuses]) => Promise.all([majors.json(), enrollment_statuses.json()]))
+          .then(([majorsData, enrollmentStatusData]) => {
+            majors.current = majorsData
+            enrollment_statuses.current = enrollmentStatusData
+            setFormData({...formData, 'enrollment_status': Object.keys(enrollmentStatusData)[0], 'major': Object.keys(majorsData)[0]})
+          })
+          .catch((err) => {
+              console.log(err);
+          });
+    },[])
 
     return (
         <div>
@@ -81,7 +98,7 @@ const AddNewApplicant = () => {
                         <Form.Control
                             type="text"
                             placeholder="Phone number"
-                            name="phonenumber"
+                            name="phone_number"
                             onChange={handleChange}
                         />
                     </FloatingLabel>
@@ -92,7 +109,11 @@ const AddNewApplicant = () => {
                         label="Major"
                         className="mb-3"
                     >
-                        <Form.Select onChange={handleChange}></Form.Select>
+                        <Form.Select onChange={handleChange} name="major">
+                          {Object.entries(majors.current).map(([key, value]) => {
+                            return (<option key={key} value={key}>{value}</option>)
+                          })}
+                        </Form.Select>
                     </FloatingLabel>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -101,13 +122,20 @@ const AddNewApplicant = () => {
                         label="Enrollment status"
                         className="mb-3"
                     >
-                        <Form.Select onChange={handleChange}></Form.Select>
+                        <Form.Select onChange={handleChange} name="enrollment_status">
+                          {Object.entries(enrollment_statuses.current).map(([key, value]) => {
+                            return (<option key={key} value={key}>{value}</option>)
+                          })}
+                        </Form.Select>
                     </FloatingLabel>
                 </Form.Group>
                 <div>
-                    <Button>Submit</Button>
+                    <Button type="submit">Submit</Button>
                 </div>
             </Form>
+            <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide>
+            <Toast.Body>{toastMessage}</Toast.Body>
+            </Toast>
         </div>
     );
 };
